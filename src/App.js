6eadf55151d3181820cfcbc2203e9168c86908df
AnasMehmood1 <anasmehmood167@gmail.com>
2024-01-15 00:03:28 +0500

    palettifyy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Colorpalette from './component/Colorpalette';
+
+function App() {
+  return (
+   <>
+   <Colorpalette/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/component/Colorpalette.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import "./style.css";
+
+const Colorpalette = () => {
+  const [color, setColor] = useState(["#EEEDF0", "#A1B5C1", "#F9ACA7", "#68747D", "#CF365F"]);
+  const [copiedColor, setCopiedColor] = useState(null);
+  const [copiedColorByC, setCopiedColorByC] = useState(null);
+  const colorutil = (length) => {
+    return Math.floor(Math.random() * length);
+  }
+
+  const handlecolor = () => {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    const newcolor = [...color];
+
+    for (let i = 0; i < 5; i++) {
+      let hexcolor = "#"
+      for (let j = 0; j < 6; j++) {
+        hexcolor += hex[colorutil(hex.length)]
+      }
+      newcolor[i] = hexcolor;
+    }
+    setColor(newcolor);
+  }
+
+  const handlecopy = (color) => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(color)
+        .then(() => {
+          setCopiedColor(color);
+          setTimeout(() => {
+            setCopiedColor(null);
+          }, 1500);
+        })
+        .catch(error => console.error('Error copying to clipboard:', error));
+    } else {
+      console.error('Clipboard API not available.');
+    }
+  }
+
+ 
+  return (
+    <>
+      <div className="color">
+        {(copiedColor && !copiedColorByC) && <p className='noti'>Color {copiedColor} copied to your clipboard</p>}
+        {copiedColorByC && <p className='noti'>Palette copied to your clipboard</p>}
+        <p className="head">Color palette generator</p>
+        <div className="color-card">
+          {
+            color.map((color, index) => (
+              <div key={index} className="color-item" onClick={() => handlecopy(color)}>
+                <div className="inner" style={{ background: color }}></div>
+                <h3>{color}</h3>
+              </div>
+            ))
+          }
+        </div>
+        <button onClick={() => handlecolor()}>Generate Palette</button>
+        <span>Or just press the "Spacebar" to generate new palettes.</span>
+        <div className="cpbtn">Click to copy individual color. Press "c" to copy the palette</div>
+      </div>
+    </>
+  );
+}
+
+export default Colorpalette;
